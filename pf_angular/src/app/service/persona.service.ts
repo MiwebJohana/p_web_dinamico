4151082a16bde3dc386b1eda9dc87ba563ec8f84
MiWebJohana <micaelareidan@gmail.com>
2023-05-15 19:21:23 -0300

    conexion front con back, model y servicios

--- pf_angular/src/app/service/persona.service.ts
+++ pf_angular/src/app/service/persona.service.ts
@@ -7,10 +7,30 @@ import { persona } from '../model/persona.model';
   providedIn: 'root'
 })
 export class PersonaService {
-  URL = "http://localhost:8080";
-  constructor(private http: HttpClient) { }
-
-  public getPersona(): Observable<persona>{
-    return this.http.get<persona>(this.URL+'/personas');
+  getPersona() {
+    throw new Error('Method not implemented.');
   }
+  URL = "http://localhost:8080/persona/";
+  constructor(private httpClient: HttpClient) { }
+
+   public list(): Observable<persona[]>{
+    return this.httpClient.get<persona[]>(this.URL+'lista');
+  }
+
+  public create(Persona: persona): Observable<any>{
+    return this.httpClient.post<any>(this.URL+'crear', persona);
+  }
+
+  public save(Persona: persona): Observable<any>{
+    return this.httpClient.post<any>(this.URL+'crear', persona);
+  }
+ 
+ 
+  public delete(id: number): Observable<any>{
+    return this.httpClient.delete<any>(this.URL+ `delete/${id}`) ;
+  }
+  public edit(Persona: persona): Observable<any>{
+    return this.httpClient.put<any>(this.URL+ 'editar', persona) ;
+  }
+
 }
